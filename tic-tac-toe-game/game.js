1d66a0839a9ad73133edc22fee943de338f98f33
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-02 12:32:54 +0700

    declare some variables and funtions for the game

--- tic-tac-toe-game/game.js
+++ tic-tac-toe-game/game.js
@@ -1,9 +1,37 @@
-const box = document.querySelectorAll('.box')
-const playerStatusTurn = document.querySelector("#player-status-turn")
-const restartBtn = document.querySelector('#restart-btn')
-const winnerCondition = [
+const cells = document.querySelectorAll(".cells");
+const textStatus = document.querySelector('#player-status-turn');
+const restartBtn = document.querySelector("#restart-btn");
+
+let holder = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameRunning = false;
+let winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-    
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
+
+
+function initializationGame() {
+}
+   
+function cellClicked() {
+
+}
+function updateCell(cell, index) {
+}
+
+function changePlayer() {
+}
+
+function checkWinner() {
+}
+
+function restartGame() {
+
+}

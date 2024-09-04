e11250800067570c8ad3d8bab786370c268bd04a
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-04 17:43:21 +0700

    add some data, shorten the code in menu

---
+++ restaurant-page/src/data.js
@@ -0,0 +1,19 @@
+const dishes = [
+    {
+        name: "Pizza",
+        descriptions: "A seafood pizza is topped with a tomato or cream-based sauce, mozzarella cheese, shrimp, calamari, clams, and sometimes mussels, garnished with garlic, fresh herbs, and a drizzle of olive oil.",
+        price: "32.99$"
+    },
+    {
+        name: "Spaghetti",
+        descriptions: "Spaghetti is a classic pasta dish served with a rich tomato sauce, often seasoned with garlic, onions, and herbs like basil and oregano, and topped with grated Parmesan cheese.",
+        price: "24.99$"
+    },
+    {
+        name: "Risotto",
+        descriptions: "Risotto is a creamy rice dish cooked with broth, often flavored with onions, garlic, and white wine, and mixed with ingredients like mushrooms, seafood, or vegetables. It's finished with Parmesan cheese and butter for richness.",
+        price: "15.99$"
+    }
+]
+
+export default dishes;

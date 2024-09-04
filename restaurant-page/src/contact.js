263b6687bf8de5505143d574e64b913382d0a5ef
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-04 17:45:51 +0700

    create contact page

--- restaurant-page/src/contact.js
+++ restaurant-page/src/contact.js
@@ -5,9 +5,32 @@ const createContactPage = () => {
     pageContent.classList.add('page-content');
 
 
-    const form = document.createElement('form');
-    form.classList.add('contact-form');
+    const heading = document.createElement('h1');
+    heading.textContent = "Contact";
+    pageContent.append(heading);
+    
+    
+    const contact = document.createElement('div');
+    contact.classList.add('contact-div');
+    pageContent.append(contact)
+    
+    const person = document.createElement('ul')
+    person.textContent = "Chef Whiskers"
+    contact.append(person)
 
+    const position = document.createElement('li')
+    position.textContent = "Head Chef"
+    person.append(position)
 
+    const phoneNumber = document.createElement('li')
+    phoneNumber.textContent = "666-1234-5461"
+    person.append(phoneNumber)
 
-}
+    const email = document.createElement('li')
+    email.textContent = "notarealeamail@fake.com"
+    person.append(email);
+
+    content.append(pageContent);
+};
+
+export default createContactPage;

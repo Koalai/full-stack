dfbd0a539d48e457e92812fa546dd63d51147885
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-14 16:44:23 +0700

    length, clear, keys,values method

--- HashMap/hashMap.js
+++ HashMap/hashMap.js
@@ -1,57 +1,77 @@
 function hashMap() {
-    let buckets = Array(16).fill(null).map(() => [])
-    let loadFactor = 0.8
-    let capacity = 0
+  let buckets = Array(16)
+    .fill(null)
+    .map(() => [])
+  let loadFactor = 0.8
+  let capacity = 0
+
+  function hash(key) {
+    let hashCode = 0
+
+    const primeNumber = 31
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + (key.charCodeAt(i) % buckets.length)
+    }
+
+    return hashCode
+  }
+
+  function set(key, value) {
+    const index = hash(key)
+    const bucket = buckets[index]
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value
+        return
+      }
+    }
+
+    bucket.push([key, value])
+    capacity++
+  }
+
+  function get(key) {
+    const index = hash(key)
+    const bucket = buckets[index]
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return (bucket[i][1] = value)
+      }
+    }
+  }
+
+  function remove(key) {
+    const index = hash(key)
+    const bucket = buckets[index]
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1)
+        capacity--
+        return true
+      }
+    }
+
+    return false
+  }
+
+  function length() {
+      return capacity;
+  }
     
-    function hash(key) {
-        let hashCode = 0;
-           
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i) % buckets.length;
-        }
-     
-        return hashCode;
-      } 
-     
-    function set(key, value) {
-        const index = hash(key);
-        const bucket = buckets[index]
-
-        for (let i = 0; i < bucket.length; i++){
-            if (bucket[i][0] === key) {
-                bucket[i][1] = value
-                return
-            }
-        }   
-
-        bucket.push([key, value]);
-        capacity++;
-
+    function clear() {
+        buckets.map(() => [])
     }
 
-    function get(key) {
-        const index = hash(key);
-        const bucket = buckets[index]
-
-        for (let i = 0; i < bucket.length; i++){
-            if (bucket[i][0] === key) {
-                return bucket[i][1] = value
-            }
-        }   
+    function keys() {
+        buckets.flatMap(bucket => bucket.map(([key]) => key))
     }
 
-    function remove(key) {
-        const index = hash(key);
-        const bucket = buckets[index]
-
-        for (let i = 0; i < bucket.length; i++){
-            if (bucket[i][0] === key) {
-                bucket.splice(i, 1)
-                return true
-            }
-        }   
-        return false
+    function values() {
+        buckets.flatMap(bucket => bucket.map(([, ...value]) => value))
     }
 
+    
 }

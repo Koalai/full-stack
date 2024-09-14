581ae12d4d19dcd39847bee798a13732b3e6e1fd
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-14 16:07:26 +0700

    hashMap, set method

---
+++ HashMap/hashMap.js
@@ -0,0 +1,33 @@
+function hashMap() {
+    let buckets = Array(16).fill(null).map(() => [])
+    let loadFactor = 0.8
+    let capacity = 0
+    
+    function hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i) % buckets.length;
+        }
+     
+        return hashCode;
+      } 
+     
+    function set(key, value) {
+        const index = hash(key);
+        const bucket = buckets[index]
+
+        for (let i = 0; i < bucket.length; i++){
+            if (bucket[i][0] === key) {
+                bucket[i][1] = value
+                return
+            }
+        }   
+
+        bucket.push([key, value]);
+        capacity++;
+
+    }
+
+}

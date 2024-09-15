36c68716b8e14581ca08311bf4bd706520080236
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-15 18:03:25 +0700

    addShip

--- Battleship/main.js
+++ Battleship/main.js
@@ -1,8 +1,8 @@
 const flipBtn = document.querySelector(".flip-btn")
 const shipOptionsContainer = document.querySelector(".ship-options-container")
-const gameContainer = document.querySelector(".game-container")
 let angleFlip = 0
 
+// flip button
 const flip = () => {
   angleFlip = angleFlip === 0 ? 90 : 0
   // console.log(shipOptionsContainer.children)
@@ -12,19 +12,24 @@ const flip = () => {
   )
 }
 
+flipBtn.addEventListener("click", flip)
+
+// game board
+const gameContainer = document.querySelector(".game-container")
 const rowNum = 10
 const colNum = 10
 const gridArr = Array.from({ length: rowNum }, () => Array(colNum).fill(null))
 
-const board = (color) => {
+const board = (color, user) => {
   const gameBoard = document.createElement("div")
   gameBoard.classList.add("game-board")
   gameBoard.style.backgroundColor = color
 
-  for (let i = 0; i < rowNum; i++) {
-    for (let j = 0; j < colNum; j++) {
+  for (let i = 1; i <= rowNum; i++) {
+    for (let j = 1; j <= colNum; j++) {
       let cell = document.createElement("div")
       cell.classList.add("cell")
+      cell.id = user
       cell.dataset.row = i
       cell.dataset.col = j
 
@@ -45,7 +50,41 @@ const board = (color) => {
   gameContainer.append(gameBoard)
 }
 
-board("yellow")
-board("pink")
+board("yellow", "Khoa")
+board("pink", "bot")
 
-flipBtn.addEventListener("click", flip)
+class Ship {
+  constructor(name, length) {
+    this.name = name
+    this.length = length
+  }
+}
+
+const ship1 = new Ship("ship1", 2)
+const ship2 = new Ship("ship2", 3)
+const ship3 = new Ship("ship3", 3)
+const ship4 = new Ship("ship4", 4)
+const ship5 = new Ship("ship5", 5)
+
+const shipContainer = [ship1, ship2, ship3, ship4, ship5]
+
+function addShip(ship) {
+    const allCell = document.querySelectorAll("#bot")
+    const randomCell = Math.floor(Math.random() * rowNum * colNum)
+    let isHorizontal = Math.random() < 0.5
+    console.log(isHorizontal)
+    console.log(randomCell)
+    let shipBlock = []
+
+    for (let i = 0; i < ship.length; i++){
+        if (randomCell % 10 === 0) {
+            return;
+        } else {
+            
+        }
+        shipBlock.push(allCell[randomCell + i])
+    }
+    console.log(shipBlock)
+}
+
+addShip(ship5)

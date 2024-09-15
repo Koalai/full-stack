b6abc46d115ad8b4e103987dc36d2b4f967deae4
Dang Khoa <dangkhoanguyen0812@gmail.com>
2024-09-15 21:19:32 +0700

    try some logic to add Ship

--- Battleship/main.js
+++ Battleship/main.js
@@ -1,90 +1,105 @@
-const flipBtn = document.querySelector(".flip-btn")
-const shipOptionsContainer = document.querySelector(".ship-options-container")
-let angleFlip = 0
+const flipBtn = document.querySelector('.flip-btn');
+const shipOptionsContainer = document.querySelector('.ship-options-container');
+let angleFlip = 0;
 
 // flip button
 const flip = () => {
-  angleFlip = angleFlip === 0 ? 90 : 0
+  angleFlip = angleFlip === 0 ? 90 : 0;
   // console.log(shipOptionsContainer.children)
-  const shipOptionsArr = Array.from(shipOptionsContainer.children)
+  const shipOptionsArr = Array.from(shipOptionsContainer.children);
   shipOptionsArr.forEach(
     (option) => (option.style.transform = `rotate(${angleFlip}deg)`)
-  )
-}
+  );
+};
 
-flipBtn.addEventListener("click", flip)
+flipBtn.addEventListener('click', flip);
 
 // game board
-const gameContainer = document.querySelector(".game-container")
-const rowNum = 10
-const colNum = 10
-const gridArr = Array.from({ length: rowNum }, () => Array(colNum).fill(null))
+const gameContainer = document.querySelector('.game-container');
+const rowNum = 10;
+const colNum = 10;
+const gridArr = Array.from({ length: rowNum }, () => Array(colNum).fill(null));
 
 const board = (color, user) => {
-  const gameBoard = document.createElement("div")
-  gameBoard.classList.add("game-board")
-  gameBoard.style.backgroundColor = color
+  const gameBoard = document.createElement('div');
+  gameBoard.classList.add('game-board');
+  gameBoard.style.backgroundColor = color;
 
   for (let i = 1; i <= rowNum; i++) {
     for (let j = 1; j <= colNum; j++) {
-      let cell = document.createElement("div")
-      cell.classList.add("cell")
-      cell.id = user
-      cell.dataset.row = i
-      cell.dataset.col = j
+      let cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.id = user;
+      cell.dataset.row = i;
+      cell.dataset.col = j;
 
-      cell.addEventListener("click", () => {
-        if (gridArr[i][j] === "clicked") {
-          console.log("Hi")
+      cell.addEventListener('click', () => {
+        if (gridArr[i][j] === 'clicked') {
+          console.log('Hi');
         }
-        gridArr[i][j] = "clicked"
+        gridArr[i][j] = 'clicked';
 
-        console.log(cell)
-        cell.style.backgroundColor = "red"
-      })
+        console.log(cell);
+        cell.style.backgroundColor = 'red';
+      });
 
-      gameBoard.append(cell)
+      gameBoard.append(cell);
     }
   }
 
-  gameContainer.append(gameBoard)
-}
+  gameContainer.append(gameBoard);
+};
 
-board("yellow", "Khoa")
-board("pink", "bot")
+board('yellow', 'Khoa');
+board('pink', 'bot');
 
 class Ship {
   constructor(name, length) {
-    this.name = name
-    this.length = length
+    this.name = name;
+    this.length = length;
   }
 }
 
-const ship1 = new Ship("ship1", 2)
-const ship2 = new Ship("ship2", 3)
-const ship3 = new Ship("ship3", 3)
-const ship4 = new Ship("ship4", 4)
-const ship5 = new Ship("ship5", 5)
+const ship1 = new Ship('ship1', 2);
+const ship2 = new Ship('ship2', 3);
+const ship3 = new Ship('ship3', 3);
+const ship4 = new Ship('ship4', 4);
+const ship5 = new Ship('ship5', 5);
 
-const shipContainer = [ship1, ship2, ship3, ship4, ship5]
+const shipContainer = [ship1, ship2, ship3, ship4, ship5];
 
 function addShip(ship) {
-    const allCell = document.querySelectorAll("#bot")
-    const randomCell = Math.floor(Math.random() * rowNum * colNum)
-    let isHorizontal = Math.random() < 0.5
-    console.log(isHorizontal)
-    console.log(randomCell)
-    let shipBlock = []
+  const allCell = document.querySelectorAll('#bot');
+  const randomCell = Math.floor(Math.random() * rowNum * colNum);
+  let isHorizontal = Math.random() < 0.5;
 
-    for (let i = 0; i < ship.length; i++){
-        if (randomCell % 10 === 0) {
-            return;
-        } else {
-            
-        }
-        shipBlock.push(allCell[randomCell + i])
+  let shipBlocks = [];
+
+  let currentRow = Math.floor(randomCell / 10);
+
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      if (randomCell + ship.length > currentRow * 10 + 10) {
+        return;
+      } else {
+        shipBlocks.push(allCell[randomCell + i]);
+      }
+    } else {
+      if (randomCell + ship.length * colNum > colNum * rowNum) {
+        return;
+      } else {
+        shipBlocks.push(allCell[randomCell + i * rowNum]);
+      }
     }
-    console.log(shipBlock)
+  }
+  shipBlocks.forEach((shipBlock) => {
+    console.log(shipBlock);
+    shipBlock.classList.add(ship.name);
+  });
 }
 
-addShip(ship5)
+addShip(ship5);
+addShip(ship1);
+addShip(ship4);
+addShip(ship3);
+addShip(ship2);

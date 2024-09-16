954a2fe817946dcccc9446237c6c3aeb92b59602
Khoa <dangkhoanguyen0812@gmail.com>
2024-09-16 21:11:09 +0700

    handle logic game, and turn

--- Battleship/main.js
+++ Battleship/main.js
@@ -1,6 +1,10 @@
 const flipBtn = document.querySelector(".flip-btn")
 const shipOptionsContainer = document.querySelector(".ship-options-container")
 const shipOptions = Array.from(shipOptionsContainer.children)
+const startBtn = document.querySelector(".start-btn")
+const infoDisplay = document.querySelector(".display-info")
+const playerTurnDisplay = document.querySelector(".player-turn-info")
+
 let angleFlip = 0
 
 // flip button
@@ -25,8 +29,8 @@ const board = (color, user) => {
   gameBoard.style.backgroundColor = color
   gameBoard.id = user
 
-  for (let i = 1; i <= rowNum; i++) {
-    for (let j = 1; j <= colNum; j++) {
+  for (let i = 0; i < rowNum; i++) {
+    for (let j = 0; j < colNum; j++) {
       let cell = document.createElement("div")
       cell.classList.add("cell")
       cell.id = `cel-${i}-${j}`
@@ -35,7 +39,6 @@ const board = (color, user) => {
 
       cell.addEventListener("click", () => {
         if (gridArr[i][j] === "clicked") {
-          console.log("Hi")
         }
         gridArr[i][j] = "clicked"
         cell.style.backgroundColor = "red"
@@ -65,7 +68,7 @@ const ship4 = new Ship("ship4", 4)
 const ship5 = new Ship("ship5", 5)
 
 const ships = [ship1, ship2, ship3, ship4, ship5]
-
+let shipDragged, notDropped
 
 function addShip(user, ship, startId) {
   const allCell = document.querySelectorAll(`#${user} div`)
@@ -73,10 +76,12 @@ function addShip(user, ship, startId) {
   let placed = false
 
   while (!placed) {
-    const randomIndexCell= Math.floor(Math.random() * rowNum * colNum)
-    const findedIndexCell = Array.from(allCell).findIndex(cell => cell.id === startId)
+    const randomIndexCell = Math.floor(Math.random() * rowNum * colNum)
+    const findedIndexCell = Array.from(allCell).findIndex(
+      (cell) => cell.id === startId
+    )
     const startCell = startId ? findedIndexCell : randomIndexCell
-    let isHorizontal = user === 'Khoa' ? angleFlip === 0 : Math.random() < 0.5
+    let isHorizontal = user === "Khoa" ? angleFlip === 0 : Math.random() < 0.5
     let shipBlocks = []
 
     if (isHorizontal) {
@@ -112,20 +117,19 @@ function addShip(user, ship, startId) {
       })
       placed = true // Ship successfully placed
     } else {
-      if (startId) break;
+      notDropped = true
+      if (startId) break
     }
   }
 }
 
-ships.forEach((ship) => addShip('bot', ship))
+ships.forEach((ship) => addShip("bot", ship))
 
 shipOptions.forEach((shipOption) => {
   shipOption.addEventListener("dragstart", dragStart)
 })
 
 const allUserBlock = document.querySelectorAll("#Khoa")
-let shipDragged, notDropped
-
 
 allUserBlock.forEach((userBlock) => {
   userBlock.addEventListener("dragover", dragOver)
@@ -148,5 +152,84 @@ function drop(e) {
   addShip("Khoa", ship, startId)
   if (!notDropped) {
     shipDragged.remove()
+  }
+}
+
+let gameOver = false
+let playerTurn
+let playerHits = []
+let botHits = []
+
+startBtn.addEventListener("click", startGame)
+function startGame() {
+  if (shipOptionsContainer.children.length !== 0) {
+    infoDisplay.textContent = "Please place all your ship into battle!"
+  } else {
+    const botAllBlocks = document.querySelectorAll("#bot div")
+    botAllBlocks.forEach((block) =>
+      block.addEventListener("click", handleClick)
+    )
+  }
+}
+
+function handleClick(e) {
+  if (!gameOver) {
+    if (e.target.classList.contains("occupied")) {
+      e.target.classList.add("boom")
+      infoDisplay.textContent = "You hit the opponent's ship"
+      let classes = Array.from(e.target.classList).filter(
+        (className) => !["cell", "boom", "occupied"].includes(className)
+      )
+
+      playerHits.push(...classes)
+      console.log(classes, playerHits)
+    } else {
+      infoDisplay.textContent = "You don't hit anything"
+      e.target.classList.add("empty")
+      playerTurn = false
+
+      setTimeout(() => {
+        botTurn()
+      }, 1000)
+    }
+  }
+}
+
+function botTurn() {
+  if (!gameOver) {
+
+    infoDisplay.textContent = "The bot is attacking"
+    playerTurnDisplay.textContent = "Let the bot go!"
+
+    setTimeout(() => {
+      let randomMove = Math.floor(Math.random() * rowNum * colNum)
+      const allPlayerBlocks = document.querySelectorAll("#Khoa div")
+      console.log(randomMove)
+      if (
+        allPlayerBlocks[randomMove].classList.contains("occupied") &&
+        allPlayerBlocks[randomMove].classList.contains("boom")
+      ) {
+        botTurn()
+        return;
+      } else if (
+        allPlayerBlocks[randomMove].classList.contains("occupied") &&
+        !allPlayerBlocks[randomMove].classList.contains("boom")
+      ) {
+        allPlayerBlocks[randomMove].classList.add("boom")
+        infoDisplay.textContent = "Bot hit your ship!"
+        let classes = Array.from(allPlayerBlocks[randomMove].classList).filter(
+          (className) => !["cell", "boom", "occupied"].includes(className)
+        )
+        botHits.push(...classes)
+      } else {
+        infoDisplay.textContent = "Bot don't hit you"
+        allPlayerBlocks[randomMove].classList.add('empty')
+        setTimeout(() => {
+          playerTurn = true;
+          playerTurnDisplay.textContent = "Khoa's turn"
+          infoDisplay.textContent = "Please take your shot!"
+        }, 1000)
+      }
+    }, 1000)
   }
 }
